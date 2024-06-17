346de90e3673782574e781fc9cb81a798723486c
Victor Hwang <135401467+victorhwang3@users.noreply.github.com>
2024-06-17 13:32:37 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,7 @@ document.getElementById("login-form").addEventListener("submit", (e) => {
     e.preventDefault();
     const password = document.getElementById("password").value;
     const hash = sha256(password);
-    const validHash = "ef92b778bafe771e89245b89ecbc88b3c2d009f6bcfc768e09cdbc55d991a6e0"; // hash da senha "senha123"
+    const validHash = "55a5e9e78207b4df8699d60886fa070079463547b095d1a05bc719bb4e6cd251"; // hash da senha "senha123"
     if (hash === validHash) {
         localStorage.setItem("authorized", "true");
         window.location.href = "index.html";
